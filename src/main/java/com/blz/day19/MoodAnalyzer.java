fa2607c2751de36b064011c72db1fa21166ff426
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-11 18:14:13 +0530

    Added :  Program for the Repeate Test Case 1.1

--- src/main/java/com/blz/day19/MoodAnalyzer.java
+++ src/main/java/com/blz/day19/MoodAnalyzer.java
@@ -3,25 +3,17 @@ package com.blz.day19;
 public class MoodAnalyzer {
     private String message;
 
+
     public MoodAnalyzer() {
     }
 
     public MoodAnalyzer(String message) {
         this.message = message;
     }
-
-    public String analyseMood() throws MoodAnalysisException {
-        try {
-
-            if (this.message.contains("Sad"))
-                return "SAD";
-            else
-                return "HAPPY";
-        } catch (Exception e) {
-            if (message == null)
-                throw new MoodAnalysisException("Invalid Mood", MoodAnalysisException.Exception_Type.NULL);
-            else
-                throw new MoodAnalysisException("Invalid Mood", MoodAnalysisException.Exception_Type.EMPTY);
-        }
+    public String analyseMood() {
+        if (this.message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
     }
 }
--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -1,4 +1,3 @@
-import com.blz.day19.MoodAnalysisException;
 import com.blz.day19.MoodAnalyzer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -7,24 +6,10 @@ public class MoodAnalyzerTest {
     @Test
     public void givenMessage_SadMood_Should_Return_Sad() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am In a Sad Mood");
-        String mood;
-        try {
-            mood = moodAnalyzer.analyseMood();
-            Assertions.assertEquals("SAD", mood);
-        } catch (MoodAnalysisException e) {
-            e.printStackTrace();
-        }
+        String mood = moodAnalyzer.analyseMood();
+        //Asserting The Result As Boolean Type True Or False
+        Assertions.assertEquals("SAD", mood);
 
-    }
-    @Test
-    public void givenMessage_AnyMood_Should_Return_HAPPY() {
-        MoodAnalyzer moodAnalyser = new MoodAnalyzer("I am in Happy Mood");
-        String mood;
-        try {
-            mood = moodAnalyser.analyseMood();
-            Assertions.assertEquals("HAPPY", mood);
-        } catch (MoodAnalysisException e) {
-            e.printStackTrace();
-        }
+
     }
 }
